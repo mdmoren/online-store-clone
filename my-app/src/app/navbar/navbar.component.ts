d0b6fbaa468d582950d0e598fcd71d7e123f22e6
mdmoren <markmoreno34@gmail.com>
2023-09-22 14:04:52 -0400

    imported font awesome icons

--- my-app/src/app/navbar/navbar.component.ts
+++ my-app/src/app/navbar/navbar.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {
+  faMagnifyingGlass,
+  faUser,
+  faCaretDown,
+  faLocationDot,
+  faShoppingBag,
+} from '@fortawesome/free-solid-svg-icons';
 
 // Define an interface to represent the structure of menu items
 interface MenuItem {
@@ -9,19 +16,25 @@ interface MenuItem {
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  faMagnifyingGlass = faMagnifyingGlass;
+  faUser = faUser;
+  faCaretDown = faCaretDown;
+  faLocationDot = faLocationDot;
+  faShoppingBag = faShoppingBag;
+
   menuItems: MenuItem[] = [
-    {name: 'Women', textColor: '#333'},
-    {name: 'Men', textColor: '#333'},
-    {name: 'Kids', textColor: '#333'},
-    {name: 'Bags & Accesories', textColor: '#333'},
-    {name: 'Athletic & Sneakers', textColor: '#333'},
-    {name: 'Shops', textColor: '#333'},
-    {name: 'Brands', textColor: '#333'},
-    {name: 'New Arrivals', textColor: '#333'},
-    {name: 'Clearance', textColor: '#C31E45'},
-    {name: 'Deals', textColor: '#C31E45'},
-  ]
+    { name: 'Women', textColor: '#333' },
+    { name: 'Men', textColor: '#333' },
+    { name: 'Kids', textColor: '#333' },
+    { name: 'Bags & Accesories', textColor: '#333' },
+    { name: 'Athletic & Sneakers', textColor: '#333' },
+    { name: 'Shops', textColor: '#333' },
+    { name: 'Brands', textColor: '#333' },
+    { name: 'New Arrivals', textColor: '#333' },
+    { name: 'Clearance', textColor: '#C31E45' },
+    { name: 'Deals', textColor: '#C31E45' },
+  ];
 }
