b9a704f488517d36a6634aaa231dbe72279bc47c
mdmoren <markmoreno34@gmail.com>
2023-09-25 15:20:53 -0400

    done

--- my-app/src/app/navbar/navbar.component.ts
+++ my-app/src/app/navbar/navbar.component.ts
@@ -5,6 +5,8 @@ import {
   faCaretDown,
   faLocationDot,
   faShoppingBag,
+  faBars,
+  faXmark
 } from '@fortawesome/free-solid-svg-icons';
 
 // Define an interface to represent the structure of menu items
@@ -31,6 +33,27 @@ export class NavbarComponent {
   faCaretDown = faCaretDown;
   faLocationDot = faLocationDot;
   faShoppingBag = faShoppingBag;
+  faBars = faBars;
+  faXmark = faXmark;
+
+  showMobileMenu = false;
+
+  toggleMobileMenu() {
+    this.showMobileMenu = !this.showMobileMenu;
+  }
+
+  // Function to toggle the display of the sub-menu
+  toggleSubMenu(menuItem: MenuItem) {
+    // Close all other sub-menus first
+    this.menuItems.forEach((item) => {
+      if (item !== menuItem) {
+        item.showSubMenu = false;
+      }
+    });
+
+    // Toggle the selected sub-menu
+    menuItem.showSubMenu = !menuItem.showSubMenu;
+  }
 
   menuItems: MenuItem[] = [
     {
@@ -38,29 +61,82 @@ export class NavbarComponent {
       textColor: '#333',
       subMenu: [
         {
-          name: 'Shoes',
-          items: ['Athletic & Sneakers', 'Boots & Booties'],
+          name: 'Women',
+          items: ['Sub menu'],
         },
       ],
     },
-    { name: 'Men', textColor: '#333', subMenu: [
-      {
-        name: 'Shoes',
-        items: ['Athletic & Sneakers', 'Boots'],
-      },
-    ], },
-    { name: 'Kids', textColor: '#333' },
-    { name: 'Bags & Accesories', textColor: '#333' },
-    { name: 'Athletic & Sneakers', textColor: '#333' },
-    { name: 'Shops', textColor: '#333' },
-    { name: 'Brands', textColor: '#333' },
+    {
+      name: 'Men',
+      textColor: '#333',
+      subMenu: [
+        {
+          name: 'Men',
+          items: ['Sub menu'],
+        },
+      ],
+    },
+    {
+      name: 'Kids',
+      textColor: '#333',
+      subMenu: [
+        {
+          name: 'Kids',
+          items: ['Sub menu'],
+        },
+      ],
+    },
+    {
+      name: 'Bags & Accesories',
+      textColor: '#333',
+      subMenu: [
+        {
+          name: 'Bags & Accesories',
+          items: ['Sub menu'],
+        },
+      ],
+    },
+    {
+      name: 'Athletic & Sneakers',
+      textColor: '#333',
+      subMenu: [
+        {
+          name: 'Athletic & Sneakers',
+          items: ['Sub menu'],
+        },
+      ],
+    },
+    {
+      name: 'Shops',
+      textColor: '#333',
+      subMenu: [
+        {
+          name: 'Shops',
+          items: ['Sub menu'],
+        },
+      ],
+    },
+    {
+      name: 'Brands',
+      textColor: '#333',
+      subMenu: [
+        {
+          name: 'Brands',
+          items: ['Sub menu'],
+        },
+      ],
+    },
     { name: 'New Arrivals', textColor: '#333' },
-    { name: 'Clearance', textColor: '#C31E45' },
+    {
+      name: 'Clearance',
+      textColor: '#C31E45',
+      subMenu: [
+        {
+          name: 'Clearance',
+          items: ['Sub menu'],
+        },
+      ],
+    },
     { name: 'Deals', textColor: '#C31E45' },
   ];
-
-    // Function to toggle the display of the sub-menu
-    toggleSubMenu(menuItem: MenuItem) {
-      menuItem.showSubMenu = !menuItem.showSubMenu;
-    }
 }
